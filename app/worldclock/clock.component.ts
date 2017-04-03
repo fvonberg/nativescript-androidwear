c9d50221ca17c3df0a38fcf5e1da2a1db96035b8
Frederik von Berg <vonberg@w11k.de>
2017-04-03 14:05:42 +0200

    added nativescript animation

--- app/worldclock/clock.component.ts
+++ app/worldclock/clock.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {Page} from "ui/page";
 import {ClockService} from "../services/clock.service";
 import {Observable} from "rxjs";
 import {ClockModel} from "../services/clock.model";
 import {SwipeDirection, SwipeGestureEventData} from "ui/gestures";
+import {View} from "ui/core/view";
 
 @Component({
   selector: 'clock',
@@ -13,6 +14,7 @@ import {SwipeDirection, SwipeGestureEventData} from "ui/gestures";
 export class ClockComponent implements OnInit {
 
   private clockModel$: Observable<ClockModel>;
+  @ViewChild("timeContainer") container: ElementRef;
 
   constructor(private page: Page, private clockService: ClockService) {
   }
@@ -26,19 +28,43 @@ export class ClockComponent implements OnInit {
     //console.log("Swipe Direction: " + args.direction);
     switch (args.direction) {
       case SwipeDirection.up:
-          this.clockService.nextTimeZone();
+          this.nextTimeZone()
           break;
       case SwipeDirection.down:
-          this.clockService.prevTimeZone();
+          this.prevTimeZone();
           break;
     }
   }
 
   nextTimeZone() {
-    this.clockService.nextTimeZone();
+    let container = <View>this.container.nativeElement;
+    let promise = container.animate({
+      translate: { x: 0, y: -200},
+      duration: 300
+    });
+    promise.then(() => {
+      this.clockService.nextTimeZone();
+      this.animateBackIn();
+    })
   }
 
   prevTimeZone() {
-    this.clockService.prevTimeZone();
+    let container = <View>this.container.nativeElement;
+    let promise = container.animate({
+      translate: { x: 0, y: 200},
+      duration: 300
+    });
+    promise.then(() => {
+      this.clockService.prevTimeZone();
+      this.animateBackIn();
+    })
+  }
+
+  animateBackIn() {
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      translate: { x: 0, y: 0},
+      duration: 500
+    })
   }
 }
